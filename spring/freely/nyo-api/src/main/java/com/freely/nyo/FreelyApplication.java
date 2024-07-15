86808221f6aad9c7321934e720545ff645c1c452
ckaanf <skywlstn777@gmail.com>
2024-07-15 10:08:32 +0900

    learn : #2 - build.gradle을 통한 버전 관리를 gradle.properties을 활용하여 한 곳에서 관리

--- spring/freely/nyo-api/src/main/java/com/freely/nyo/FreelyApplication.java
+++ spring/freely/nyo-api/src/main/java/com/freely/nyo/FreelyApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(value = {"com.freely.nyo", "com.freely.nyocore.config"})
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.freely.nyo", "com.freely.nyocore.config"})
 public class FreelyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FreelyApplication.class, args);
