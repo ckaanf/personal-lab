0d3b7244f9af7644f4aab4d4aa7806fe06124d2e
ckaanf <skywlstn777@gmail.com>
2024-10-18 11:02:12 +0900

    learn : #6 - createOrder() refactoring

--- spring/cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ spring/cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -29,44 +29,23 @@ public class OrderService {
 	private final OrderRepository orderRepository;
 	private final StockRepository stockRepository;
 
+	/**
+	 * 재고 감소 -> 동시성 고민
+	 * 비관적 락/ 낙관적 락
+	 */
 	public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
 		// Product
 		List<String> productNumbers = request.getProductNumbers();
 		List<Product> products = findProductBy(productNumbers);
 
-		// 재고 차감 체크가 필요한 상품들 filter
-		List<String> stockProductNumbers = products.stream()
-			.filter(product -> ProductType.containsStockType(product.getType()))
-			.map(Product::getProductNumber)
-			.toList();
-
-		// 재고 엔티티 조회
-		List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
-		Map<String, Stock> stockMap = stocks.stream()
-			.collect(Collectors.toMap(Stock::getProductNumber, stock -> stock));
-
-		// 상품별 counting
-		Map<String, Long> productCountingMap = stockProductNumbers.stream()
-			.collect(Collectors.groupingBy(p -> p, Collectors.counting()));
-
-		// 재고 차감 시도
-		for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
-			Stock stock = stockMap.get(stockProductNumber);
-			int quantity = productCountingMap.get(stockProductNumber).intValue();
-
-			// deductQuantity 안에서 isQuantityLessThan이 있는데, 왜 굳이 두번 체크?
-			if (stock.isQuantityLessThan(quantity)) {
-				throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
-			}
-			stock.deductQuantity(quantity);
-
-		}
+		deductStockQuantities(products);
 
 		// Order
 		Order order = Order.create(products, registeredDateTime);
 		Order savedOrder = orderRepository.save(order);
 
 		return OrderResponse.of(savedOrder);
+
 	}
 
 	private List<Product> findProductBy(List<String> productNumbers) {
@@ -79,5 +58,42 @@ public class OrderService {
 		return productNumbers.stream()
 			.map(productMap::get)
 			.toList();
+
+	}
+
+	private void deductStockQuantities(List<Product> products) {
+		List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+		Map<String, Stock> stockMap = createStockMapBy(stockProductNumbers);
+		Map<String, Long> productCountingMap = createCountingMapBy(stockProductNumbers);
+
+		for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+			Stock stock = stockMap.get(stockProductNumber);
+			int quantity = productCountingMap.get(stockProductNumber).intValue();
+
+			if (stock.isQuantityLessThan(quantity)) {
+				throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+			}
+			stock.deductQuantity(quantity);
+		}
+
+	}
+
+	private List<String> extractStockProductNumbers(List<Product> products) {
+		return products.stream()
+			.filter(product -> ProductType.containsStockType(product.getType()))
+			.map(Product::getProductNumber)
+			.toList();
+	}
+
+	private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
+		List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+		return stocks.stream()
+			.collect(Collectors.toMap(Stock::getProductNumber, stock -> stock));
+	}
+
+	private Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
+		return stockProductNumbers.stream()
+			.collect(Collectors.groupingBy(p -> p, Collectors.counting()));
 	}
 }
