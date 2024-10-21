deb22b3e14f4387d8113441db60ddc3edc43167a
ckaanf <skywlstn777@gmail.com>
2024-10-21 16:24:19 +0900

    learn : #6 - Presentation Layer 테스트 (2)

--- spring/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ spring/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -2,11 +2,15 @@ package sample.cafekiosk.spring.api.controller.product;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.spring.api.ApiResponse;
 import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
@@ -18,14 +22,12 @@ public class ProductController {
 	private final ProductService productService;
 
 	@PostMapping("/api/v1/products/new")
-	public void createProduct(ProductCreateRequest request) {
-
-		productService.createProduct(request);
-
+	public ApiResponse<ProductResponse> createProduct(@Valid @RequestBody ProductCreateRequest request) {
+		return ApiResponse.ok(productService.createProduct(request));
 	}
 
 	@GetMapping("/api/v1/products/selling")
-	public List<ProductResponse> getSellingProducts() {
-		return productService.getSellingProducts();
+	public ApiResponse<List<ProductResponse>> getSellingProducts() {
+		return ApiResponse.ok(productService.getSellingProducts());
 	}
 }
--- spring/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
+++ spring/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -1,17 +1,35 @@
 package sample.cafekiosk.spring.api.controller.product.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
 @Getter
+@NoArgsConstructor
 public class ProductCreateRequest {
 
+	@NotNull(message = "상품 타입은 필수입니다.")
 	private ProductType type;
+
+	@NotNull(message = "상품 판매상태는 필수입니다.")
 	private ProductSellingStatus sellingStatus;
+
+	// String name -> 상품 이름은 20자 제한
+	@NotBlank(message = "상품 이름은 필수입니다.")
+	// @Max(20) // 이걸 여기서 하는게 맞나..? 고민해보기
+	// @NotNull // "", " " 통과됨
+	// @NotEmpty // "  " 통과됨
 	private String name;
+
+	@Positive(message = "상품 가격은 양수여야 합니다.")
 	private int price;
 
 	@Builder
