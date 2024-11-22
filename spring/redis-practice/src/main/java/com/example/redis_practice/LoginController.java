84830c5eea4059a98c96bc746d1c342cd18cc1ca
ckaanf <skywlstn777@gmail.com>
2024-11-22 14:38:56 +0900

    learn : #18 - session clustering

---
+++ spring/redis-practice/src/main/java/com/example/redis_practice/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.redis_practice;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/api/v1")
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login(HttpSession sesssion, @RequestParam String name) {
+		sesssion.setAttribute("name", name);
+		return "saved.";
+	}
+
+	@GetMapping("/my-name")
+	public String myName(HttpSession sesssion) {
+		return (String) sesssion.getAttribute("name");
+	}
+}
