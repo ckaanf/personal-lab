75856dc6c622eae2919d9d18d34d6231045f7f50
Jin Soo Kim <57103133+ckaanf@users.noreply.github.com>
2025-03-05 15:23:14 +0900

    ThreadPoolTaskExecutor (#27)
    * task decorator 활용 -> yaml에는 값이 없음
    
    * task decorator를 빈으로 등록

---
+++ spring-lab/src/main/java/org/example/springlab/threadpool/ThreadPoolController.java
@@ -0,0 +1,26 @@
+package org.example.springlab.threadpool;
+
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class ThreadPoolController {
+	private final ThreadPoolTarget threadPoolTarget;
+
+	public ThreadPoolController(ThreadPoolTarget threadPoolTarget) {
+		this.threadPoolTarget = threadPoolTarget;
+
+	}
+
+	@GetMapping("/threadPool")
+	public String threadPool() {
+		MDC.put("userId", "helloWorld");
+		log.info("threadPool. userId: {}", MDC.get("userId"));
+		threadPoolTarget.async();
+		return "ok";
+	}
+}
