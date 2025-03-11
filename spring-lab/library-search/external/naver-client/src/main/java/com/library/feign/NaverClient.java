7f4b7a026597cfecd713fa0e05b89923929765f8
ckaanf <skywlstn777@gmail.com>
2025-03-11 16:36:03 +0900

    [LS-4] 외부 api 연동 - naver response

--- spring-lab/library-search/external/naver-client/src/main/java/com/library/feign/NaverClient.java
+++ spring-lab/library-search/external/naver-client/src/main/java/com/library/feign/NaverClient.java
@@ -1,5 +1,6 @@
 package com.library.feign;
 
+import com.library.NaverBookResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface NaverClient {
 
 	@GetMapping("/v1/search/book.json")
-	String searchBook(@RequestParam("query") String query,
-					  @RequestParam("start") int start,
-					  @RequestParam("display") int display);
+	NaverBookResponse searchBook(@RequestParam("query") String query,
+								 @RequestParam("start") int start,
+								 @RequestParam("display") int display);
 
 }
