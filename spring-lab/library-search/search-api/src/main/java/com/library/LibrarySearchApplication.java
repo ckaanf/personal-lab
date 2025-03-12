08d6e7df760b33c88ebff9ae5f4728de111ba55b
ckaanf <skywlstn777@gmail.com>
2025-03-12 14:06:11 +0900

    [LS-5] api 서버 구현 - tmp

---
+++ spring-lab/library-search/search-api/src/main/java/com/library/LibrarySearchApplication.java
@@ -0,0 +1,14 @@
+package com.library;
+
+import com.library.feign.NaverClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(clients = NaverClient.class)
+@SpringBootApplication
+public class LibrarySearchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySearchApplication.class, args);
+	}
+}
