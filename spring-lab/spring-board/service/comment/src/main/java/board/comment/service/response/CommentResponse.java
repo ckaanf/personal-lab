988bf65ad9e1cb88465100d5114de6506d3a66d3
Jinsoo Kim <57103133+ckaanf@users.noreply.github.com>
2025-04-04 14:09:03 +0900

    #39 comment paging infinite (#41)
    * chore : coderabbit yaml 추가
    
    * feature : #39 - 무한 depth 댓글

--- spring-lab/spring-board/service/comment/src/main/java/board/comment/service/response/CommentResponse.java
+++ spring-lab/spring-board/service/comment/src/main/java/board/comment/service/response/CommentResponse.java
@@ -1,6 +1,7 @@
 package board.comment.service.response;
 
 import board.comment.entity.Comment;
+import board.comment.entity.CommentV2;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -15,6 +16,7 @@ public class CommentResponse {
     private Long articleId;
     private Long writerId;
     private Boolean deleted;
+    private String path;
     private LocalDateTime createdAt;
 
     public static CommentResponse from(Comment comment) {
@@ -25,6 +27,18 @@ public class CommentResponse {
         response.articleId = comment.getArticleId();
         response.writerId = comment.getWriterId();
         response.deleted = comment.getDeleted();
+        response.createdAt = comment.getCreatedAt();
+        return response;
+    }
+
+    public static CommentResponse from(CommentV2 comment) {
+        CommentResponse response = new CommentResponse();
+        response.commentId = comment.getCommentId();
+        response.content = comment.getContent();
+        response.articleId = comment.getArticleId();
+        response.writerId = comment.getWriterId();
+        response.deleted = comment.getDeleted();
+        response.path = comment.getCommentPath().getPath();
         response.createdAt = comment.getCreatedAt();
         return response;
     }
