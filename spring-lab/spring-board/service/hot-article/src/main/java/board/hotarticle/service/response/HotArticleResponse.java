70cd253e34572dfdfb167e1cc655a9a2e5ab882f
ckaanf <skywlstn777@gmail.com>
2025-04-17 13:39:23 +0900

    feature : #50 - 인기글 Consumer 구현 - Producer&Consumer 테스트

--- spring-lab/spring-board/service/hot-article/src/main/java/board/hotarticle/service/response/HotArticleResponse.java
+++ spring-lab/spring-board/service/hot-article/src/main/java/board/hotarticle/service/response/HotArticleResponse.java
@@ -2,10 +2,12 @@ package board.hotarticle.service.response;
 
 import board.hotarticle.client.ArticleClient;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
+@ToString
 public class HotArticleResponse {
     private Long articleId;
     private String title;
